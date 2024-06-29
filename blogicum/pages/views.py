b748464ac700ddac03c87d802590ab2593c6a671
Mihhail <ktunec5@gmail.com>
2024-06-29 12:50:58 +0100

    static_ignored

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def rules(request):
+    template = 'pages/rules.html'
+    return render(request, template)
+
+
+def about(request):
+    template = 'pages/about.html'
+    return render(request, template)
